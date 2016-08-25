cb164e9be1afcb954952a79b5d4858c504fd9749
Hannah Stewart <hmstewart84@gmail.com>
2016-08-25 15:21:04 +0100

    Initial commit

---
+++ specs/DeckTest.java
@@ -0,0 +1,31 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+import game.*;
+
+public class DeckTest {
+
+  Card card;
+  Deck deck;
+
+  @Before 
+  public void before(){
+    card = new Card(SuitType.DIAMONDS, RankType.TWO);
+    deck = new Deck();
+  }
+
+  @Test
+  public void canAddCard(){
+    deck.addCard(card);
+    int result = deck.cardCount();
+    assertEquals(result, 1);
+  }
+
+  @Test
+  public void canCreateFullDeck() {
+    deck.createFullDeck();
+    int result = deck.cardCount();
+    assertEquals(52, result);
+  }
+
+
+}
